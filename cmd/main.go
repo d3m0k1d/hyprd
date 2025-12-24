a9340585bac688be635eeca5efa100ce5a7d83ac
d3m0k1d <d3m0k1d1337@gmail.com>
2025-12-25 00:05:32 +0300

    Add docs and update actions logic

--- cmd/main.go
+++ cmd/main.go
@@ -28,13 +28,13 @@ func Execute() {
 }
 
 func main() {
-	logger := logger.New(false)
+	log := logger.New(false)
 	Init()
 	Execute()
 	cfg, err := config.LoadConfig()
 
 	if err != nil {
-		logger.Error("Failed to load config", "err", err)
+		log.Error("Failed to load config", "err", err)
 		os.Exit(1)
 	}
 
@@ -42,44 +42,58 @@ func main() {
 	for _, rule := range cfg.Rules {
 		triggerIndex[rule.Trigger] = append(triggerIndex[rule.Trigger], rule)
 	}
-	logger.Info("Loaded rules", "count", len(cfg.Rules))
+	log.Info("Loaded rules", "count", len(cfg.Rules))
 
 	listener := &ipc.Listener{}
 	eventsChan, err := listener.Start()
 	if err != nil {
-		logger.Error("Failed to start listener", "err", err)
+		log.Error("Failed to start listener", "err", err)
 		os.Exit(1)
 	}
-	logger.Info("Event listener started")
+	log.Info("Event listener started")
 
 	for {
 		select {
 		case event := <-eventsChan:
-			logger.Info("Received event", "type", event.Event)
-
 			if rules, exists := triggerIndex[event.Event]; exists {
 				for _, rule := range rules {
-					logger.Info("Executing rule", "name", rule.Name)
-					executeActions(rule.Actions, event)
+					log.Info("Executing rule", "name", rule.Name)
+					executeActions(rule.Actions, event, *log)
 				}
 			}
 		}
 	}
 }
 
-func executeActions(actions []string, event ipc.Event) {
-	logger := logger.New(false)
+func executeActions(actions []string, event ipc.Event, log logger.Logger) {
+	log.Info("Starting actions execution", "count", len(actions))
 
-	for _, action := range actions {
-		logger.Info("Executing action", "action", action)
+	for i, action := range actions {
+		log.Info("Executing action", "index", i, "action", action)
 
 		cmd := exec.Command("sh", "-c", action)
+		cmd.Env = os.Environ()
+		cmd.Dir = os.Getenv("HOME")
+
 		output, err := cmd.CombinedOutput()
 
+		log.Info("Command output",
+			"action", action,
+			"output", string(output),
+			"exit_code", cmd.ProcessState)
+
 		if err != nil {
-			logger.Error("Action failed", "action", action, "err", err, "output", string(output))
+			log.Error("Action failed",
+				"action", action,
+				"err", err.Error(),
+				"output", string(output),
+				"exit_code", cmd.ProcessState)
 		} else {
-			logger.Debug("Action succeeded", "action", action)
+			log.Info("Action succeeded",
+				"action", action,
+				"output", string(output))
 		}
 	}
+
+	log.Info("Finished actions execution")
 }
