ebcb61995ccbe06b546dce9434a10afeace36803
d3m0k1d <d3m0k1d1337@gmail.com>
2025-12-24 22:30:57 +0300

    Update config.go logic and add parse events with actions in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/d3m0k1d/hyprd/pkg/config"
 	"github.com/d3m0k1d/hyprd/pkg/ipc"
+	"github.com/d3m0k1d/hyprd/pkg/logger"
 	"github.com/spf13/cobra"
 	"os"
+	"os/exec"
 )
 
 var rootCmd = &cobra.Command{
@@ -25,17 +28,58 @@ func Execute() {
 }
 
 func main() {
+	logger := logger.New(false)
 	Init()
 	Execute()
-	listner := &ipc.Listener{}
-	eventsChan, err := listner.Start()
+	cfg, err := config.LoadConfig()
+
 	if err != nil {
+		logger.Error("Failed to load config", "err", err)
 		os.Exit(1)
 	}
+
+	triggerIndex := make(map[string][]config.Rule)
+	for _, rule := range cfg.Rules {
+		triggerIndex[rule.Trigger] = append(triggerIndex[rule.Trigger], rule)
+	}
+	logger.Info("Loaded rules", "count", len(cfg.Rules))
+
+	listener := &ipc.Listener{}
+	eventsChan, err := listener.Start()
+	if err != nil {
+		logger.Error("Failed to start listener", "err", err)
+		os.Exit(1)
+	}
+	logger.Info("Event listener started")
+
 	for {
 		select {
-		case eventsChan := <-eventsChan:
-			fmt.Println(eventsChan)
+		case event := <-eventsChan:
+			logger.Info("Received event", "type", event.Event)
+
+			if rules, exists := triggerIndex[event.Event]; exists {
+				for _, rule := range rules {
+					logger.Info("Executing rule", "name", rule.Name)
+					executeActions(rule.Actions, event)
+				}
+			}
+		}
+	}
+}
+
+func executeActions(actions []string, event ipc.Event) {
+	logger := logger.New(false)
+
+	for _, action := range actions {
+		logger.Info("Executing action", "action", action)
+
+		cmd := exec.Command("sh", "-c", action)
+		output, err := cmd.CombinedOutput()
+
+		if err != nil {
+			logger.Error("Action failed", "action", action, "err", err, "output", string(output))
+		} else {
+			logger.Debug("Action succeeded", "action", action)
 		}
 	}
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,24 +1,74 @@
 package config
 
 import (
-	"github.com/BurntSushi/toml"
 	"os"
+	"path/filepath"
+
+	"github.com/BurntSushi/toml"
+	"github.com/d3m0k1d/hyprd/pkg/logger"
 )
 
 type Config struct {
-	Rules []Rule `toml:"rules"`
+	Rules []Rule `toml:"rule"`
 }
 
 type Rule struct {
-	Name       string   `toml:"name"`
-	Trigger    string   `toml:"trigger"`
-	Conditions []string `toml:"conditions"`
-	Actions    []string `toml:"actions"`
+	Name    string   `toml:"name"`
+	Trigger string   `toml:"trigger"`
+	Actions []string `toml:"actions"`
 }
 
-func LoadConfig(path string) (*Config, error) {
-	data, _ := os.ReadFile(path)
+func LoadConfig() (*Config, error) {
+	logger := logger.New(false)
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		logger.Error("Error getting user home directory", "err", err)
+		return nil, err
+	}
+
+	configDir := filepath.Join(homeDir, ".config", "hyprd")
+	configPath := filepath.Join(configDir, "config.toml")
+
+	if err := os.MkdirAll(configDir, 0755); err != nil {
+		logger.Error("Failed to create config directory", "err", err)
+		return nil, err
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		logger.Info("Config file not found, creating default", "path", configPath)
+		if err := createDefaultConfig(configPath); err != nil {
+			logger.Error("Failed to create default config", "err", err)
+			return nil, err
+		}
+	}
+
+	data, err := os.ReadFile(configPath)
+	if err != nil {
+		logger.Error("Failed to read config file", "err", err)
+		return nil, err
+	}
+
 	var cfg Config
-	toml.Unmarshal(data, &cfg)
+	if err := toml.Unmarshal(data, &cfg); err != nil {
+		logger.Error("Failed to parse config", "err", err)
+		return nil, err
+	}
+
 	return &cfg, nil
+}
+
+func createDefaultConfig(path string) error {
+	defaultConfig := `# Hyprd Configuration
+
+[[rule]]
+name = "example-rule"
+trigger = ""
+actions = ["reboot"]
+
+[[rule]]
+name = "terminal-rule"
+trigger = ""
+actions = ["kitty"]
+`
+	return os.WriteFile(path, []byte(defaultConfig), 0644)
 }
